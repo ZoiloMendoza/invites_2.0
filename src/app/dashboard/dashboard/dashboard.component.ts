132d0b3c956f69ce8787fb7e19901ab5073d1a94
ZoiloMendoza <zoilomm@hotmail.com>
2024-01-19 03:16:02 -0600

    add busqueda

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,10 +1,66 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
+  imports: [CommonModule, FormsModule],
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  tableHeaders = [
+    'Invitado',
+    'Apellido',
+    '# Lugares',
+    'Whatsapp',
+    'Envio',
+    'Status',
+    'Invitacion Web',
+    'Acciones',
+  ];
+
+  tableData = [
+    [
+      'Alison',
+      'Rangel',
+      '3',
+      '+5218888888888',
+      'Sí',
+      'Confirmado',
+      'Link',
+      '[Editar] [Eliminar]',
+    ],
+    [
+      'Zoy',
+      'Mendoza',
+      '1',
+      '+5219999999999',
+      'No',
+      'Pendiente',
+      'Link',
+      '[Editar] [Eliminar]',
+    ],
+    [
+      'Daniel',
+      'Escareño',
+      '2',
+      '+521777777777',
+      'No',
+      'Cancelado',
+      'Link',
+      '[Editar] [Eliminar]',
+    ],
+  ];
+
+  searchTerm: string = '';
+
+  get filteredTableData(): string[][] {
+    return this.tableData.filter((row) =>
+      row.some((cell) =>
+        cell.toLowerCase().includes(this.searchTerm.toLowerCase()),
+      ),
+    );
+  }
+}
