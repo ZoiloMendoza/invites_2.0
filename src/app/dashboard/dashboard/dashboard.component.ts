cb089289f7da5b27ed0b818807f49a2db5aede28
ZoiloMendoza <zoilomm@hotmail.com>
2024-01-18 23:56:45 -0600

    update dashboard

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-dashboard',
   standalone: true,
   imports: [],
-  exports: [DashboardComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
